8f26a557d2b1571895e52c44165283fef34b4b2e
Dino <kill_death@163.com>
2018-04-07 23:30:27 +0800

    Demo初步搭建

---
+++ FactoryDemo/FactoryDemo/Controller/DORootVC.m
@@ -0,0 +1,27 @@
+//
+//  DORootVC.m
+//  FactoryDemo
+//
+//  Created by 魏欣宇 on 2018/4/7.
+//  Copyright © 2018年 Dino. All rights reserved.
+//
+
+#import "DORootVC.h"
+
+@interface DORootVC ()
+
+@end
+
+@implementation DORootVC
+
+#pragma mark - Life Cycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"工厂模式";
+    
+    self.view.backgroundColor = [UIColor redColor];
+    
+}
+
+@end
