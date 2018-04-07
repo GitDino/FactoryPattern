f99620d8de41a416b2707d72a129c856b0fbe6de
Dino <kill_death@163.com>
2018-04-08 02:07:08 +0800

    Demo初步搭建

---
+++ AbstractFactoryDemo/AbstractFactoryDemo/Controller/DoRootVC.m
@@ -0,0 +1,25 @@
+//
+//  DoRootVC.m
+//  AbstractFactoryDemo
+//
+//  Created by 魏欣宇 on 2018/4/8.
+//  Copyright © 2018年 Dino. All rights reserved.
+//
+
+#import "DoRootVC.h"
+
+@interface DoRootVC ()
+
+@end
+
+@implementation DoRootVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"抽象工厂";
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+@end
