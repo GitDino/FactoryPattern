ceb157c5a97a324fc880b0046ebb09a7a1f3a300
Dino <kill_death@163.com>
2018-04-07 22:57:39 +0800

    Demo初步搭建

---
+++ SimpleFactoryDemo/SimpleFactoryDemo/Controller/DORootVC.m
@@ -0,0 +1,26 @@
+//
+//  DORootVC.m
+//  SimpleFactoryDemo
+//
+//  Created by 魏欣宇 on 2018/4/7.
+//  Copyright © 2018年 Dino. All rights reserved.
+//
+
+#import "DORootVC.h"
+
+@interface DORootVC ()
+
+@end
+
+@implementation DORootVC
+
+#pragma mark - Life Cycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"简单工厂";
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+@end
